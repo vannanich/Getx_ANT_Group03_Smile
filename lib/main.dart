a20e73dd11bf22d9c72df512e13186aee13d8cfe
Kao Meyly <kaomeyly@gmail.com>
2026-05-16 13:19:00 +0700

    pull code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/routes/app_pages.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+// ignore: unused_import
 import 'app/routes/app_routes.dart';
 
 void main() {
