b1805f21312e6e9a5dd7b3ee81f43c003a5c2742
vannanich <vannanich2@email.com>
2026-05-27 10:33:17 +0700

    test scan id

--- lib/app/modules/screen/doctor_feature/d_homescreen/d_homescreen_binding.dart
+++ lib/app/modules/screen/doctor_feature/d_homescreen/d_homescreen_binding.dart
@@ -3,7 +3,7 @@
 import 'package:flutter_application_1/app/modules/screen/doctor_feature/d_homescreen/d_homescreen_controller.dart';
 import 'package:get/get.dart';
 
-class DoctorHomeBinding extends Bindings {
+class DHomescreenBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<DoctorHomeController>(() => DoctorHomeController());
